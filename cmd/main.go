10ff9a7ddeef8af6bd2be2f3f2b887ce32d728f9
theghostmac <theghostmac@gmail.com>
2024-01-30 18:50:48 +0100

    [infra] fixing the deploy logs

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/theghostmac/donorwallet/internal/apis"
 	"github.com/theghostmac/donorwallet/internal/database"
 	"go.uber.org/zap"
@@ -12,9 +14,14 @@ func main() {
 	database.InitDB()
 	defer database.CloseDB()
 
-	port := ":6569"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	logger.Info("Starting server on default port.", zap.String("port", port))
 	router := apis.InitRouter()
-	router.Run(port)
+	if err := router.Run(":" + port); err != nil {
+        logger.Fatal("error: %s", zap.Error(err))
+	}
 }
